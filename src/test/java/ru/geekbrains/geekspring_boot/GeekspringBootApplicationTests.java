3500ff1e6a1a3687446995cab64f307d55c85042
RegisEmiel <regisemiel@yandex.ru>
2021-12-22 22:52:13 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/geekspring_boot/GeekspringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.geekspring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekspringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
